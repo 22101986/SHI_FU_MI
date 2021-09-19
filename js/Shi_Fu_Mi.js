5769e874ae60b80ee7165edcd96168266757a678
harold Pierrache <h.pierrache@gmail.com>
2021-09-19 16:40:26 +0200

    secondcommit

--- js/Shi_Fu_Mi.js
+++ js/Shi_Fu_Mi.js
@@ -1,16 +1,20 @@
 alert("Salut!! Ca vous dit une petite partie de SHI-FU-MI!??!");
+// affiche un message a l'utilisateur //
 
 var userName = prompt("Ok! Pour jouer entrez votre nom: ");
 
 var userChoice = prompt("Alors " + userName + "... pierre, feuille ou ciseaux??");
+// l'utilisateur entre des données qui sont stockée dans var userName  puis userChoice //
 
 alert(userChoice + "!! super choix " + userName + "!! Maintenant à l'ordinateur de choisir...");
+// affiche les données entrées par l'utilisateur dans le message alert() //
 
 var array = ["pierre","feuille","ciseaux"];
-
 var choice = array[Math.floor(Math.random()*array.length)];
+// permet à l'ordinateur de choisir au hasard un élément de array //
 
 alert("L'ordinateur à choisi " + choice);
+// message renvoyant le choix de l'ordinateur //
 
 function result(userChoice, choice) {
     if(choice === "pierre" && userChoice === "ciseaux"){
